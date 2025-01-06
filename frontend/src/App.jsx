025d9fffbf2964599125c18997f51ae807585267
Satyam Kumar <sk9161511745@gmail.com>
2025-01-06 23:16:58 +0530

    added basic files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Topbar from './Components/Topbar'
+function App() {
+
+  return (
+    <>
+      <div className="flex justify-center" >
+        <Topbar />
+      </div>
+    </>
+  )
+}
+
+export default App
