a62951884b166342568b0c7698d6397f4d7af226
Satyam Kumar <sk9161511745@gmail.com>
2025-01-08 23:39:53 +0530

    created frontend file setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-import Topbar from './Components/Topbar'
+
 function App() {
 
   return (
     <>
-      <div className="flex justify-center" >
-        <Topbar />
-      </div>
+     
     </>
   )
 }
