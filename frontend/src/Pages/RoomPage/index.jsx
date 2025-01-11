9e14817dd429fd7f3cf6939ea2be7d8d86ee64a7
Satyam Kumar <sk9161511745@gmail.com>
2025-01-11 23:11:06 +0800

    started whiteBoard ui

--- frontend/src/Pages/RoomPage/index.jsx
+++ frontend/src/Pages/RoomPage/index.jsx
@@ -1,50 +1,85 @@
 import { useState } from "react";
+import { IoIosUndo } from "react-icons/io";
+import { IoIosRedo } from "react-icons/io";
+import { FaPencil } from "react-icons/fa6";
+import { FaEraser } from "react-icons/fa";
+import { IoIosColorPalette } from "react-icons/io";
+import { FaShapes } from "react-icons/fa6";
 
 const RoomPage = () => {
-  const [tool, setTool] = useState("pencil");
-  
-  return (
-    <div className="row">
-      <h1 className="text-center py-4">White Board Sharing App</h1>
-      <div className="col-md-12 mt-4 mb-5 d-flex items-center justify-center">
-        <div className=" bg-gray-300 d-flex col-md-4 justify-center gap-5">
-          <label className="flex items-center ">
-            <input
-              type="radio"
-              name="tool"
-              value="pencil"
-              onChange={(e) => setTool(e.target.value)}
-              className="cursor-pointer"
-            />
-            Pencil
-          </label>
-          <label className="flex items-center ">
-            <input
-              type="radio"
-              name="tool"
-              value="line"
-              onChange={(e) => setTool(e.target.value)}
-              className="cursor-pointer"
-            />
-            Line
-          </label>
-          <label className="flex items-center ">
-            <input
-              type="radio"
-              name="tool"
-              value="rectangle"
-              onChange={(e) => setTool(e.target.value)}
-              className="cursor-pointer"
-            />
-            Rectangle
-          </label>
-        </div>
-        <div>
+     const [tool, setTool] = useState("pencil");
 
-        </div>
-      </div>
-    </div>
-  );
+     return (
+          <div className="bg-slate-400 flex flex-col h-screen items-center">
+               <div className="bg-white flex justify-between items-center m-2 rounded-full border-slate-950 font-mono text-1xl h-12 w-4/6 text-gray-500  px-2">
+                    
+                    <div className="flex gap-2 justify-start items-center px-2">
+                         <IoIosUndo
+                              className="cursor-pointer h-5 w-5"
+                         />
+                         <IoIosRedo
+                              className="cursor-pointer h-5 w-5"
+                         />
+                    </div>
+                    <div className="flex justify-center items-center gap-2 ">
+                    <FaPencil
+                    className="cursor-pointer h-5 w-5" />
+                    <FaEraser
+                    className="cursor-pointer h-5 w-5" />
+                    <IoIosColorPalette 
+                    className="cursor-pointer h-5 w-5"
+                    />
+                    <FaShapes
+                    className="cursor-pointer h-5 w-5" />
+
+                    </div>
+                    <div className="flex gap-1 justify-end">
+                    <button className="bg-red-500 px-2 rounded-full text-white h-8"
+                    >
+                   Clear
+                    </button>
+                        
+                    </div>
+               </div>
+          </div>
+     );
 };
 
 export default RoomPage;
+
+// <div className="col-md-12 mt-4 mb-5 d-flex items-center justify-center">
+//         <div className=" bg-gray-300 d-flex col-md-4 justify-center gap-5">
+//           <label className="flex items-center ">
+//             <input
+//               type="radio"
+//               name="tool"
+//               value="pencil"
+//               onChange={(e) => setTool(e.target.value)}
+//               className="cursor-pointer"
+//             />
+//             Pencil
+//           </label>
+//           <label className="flex items-center ">
+//             <input
+//               type="radio"
+//               name="tool"
+//               value="line"
+//               onChange={(e) => setTool(e.target.value)}
+//               className="cursor-pointer"
+//             />
+//             Line
+//           </label>
+//           <label className="flex items-center ">
+//             <input
+//               type="radio"
+//               name="tool"
+//               value="rectangle"
+//               onChange={(e) => setTool(e.target.value)}
+//               className="cursor-pointer"
+//             />
+//             Rectangle
+//           </label>
+//         </div>
+//         <div>
+//         </div>
+//       </div>
